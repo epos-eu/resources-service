671de98939055270a4be8eb9ff681d645850b517
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2024-02-12 09:49:38 +0100

    update

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,29 +1,12 @@
-import static org.epos.handler.dbapi.util.DBUtil.getFromDB;
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import javax.persistence.EntityManager;
-
-import org.epos.api.beans.DiscoveryItem;
 import org.epos.api.core.EnvironmentVariables;
 import org.epos.api.core.ZabbixExecutor;
-import org.epos.api.core.distributions.DistributionDetailsGenerationJPA;
-import org.epos.api.core.distributions.DistributionSearchGenerationJPA;
-import org.epos.api.core.facilities.FacilityDetailsItemGenerationJPA;
-import org.epos.api.core.facilities.FacilitySearchGenerationJPA;
 import org.epos.api.core.organizations.OrganisationsGeneration;
 import org.epos.api.facets.Facets;
-import org.epos.api.facets.FacetsGeneration;
-import org.epos.api.facets.FacetsNodeTree;
-import org.epos.handler.dbapi.model.EDMFacility;
-import org.epos.handler.dbapi.service.DBService;
-
 import com.google.gson.Gson;
-import com.jayway.jsonpath.internal.Utils;
 
 public class Tests {
 
@@ -37,7 +20,8 @@ public class Tests {
 		}
 		
 		Map<String, Object> headers = new HashMap<String, Object>();
-		headers.put("type", "facilitiesproviders");
+		headers.put("type", "dataproviders");
+		headers.put("country", "IT");
 		
 		try {
 			System.out.println(Facets.getInstance().generateFacetsFromDatabase());
